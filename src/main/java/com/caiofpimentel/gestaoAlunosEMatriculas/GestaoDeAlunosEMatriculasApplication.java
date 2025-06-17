415bf19358bebb6702c2c7f7c139dcb2ba829a35
Caio <caio-pimentel@hotmail.com>
2025-06-17 19:07:08 -0300

    Initial commit

---
+++ src/main/java/com/caiofpimentel/gestaoAlunosEMatriculas/GestaoDeAlunosEMatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.caiofpimentel.gestaoAlunosEMatriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeAlunosEMatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeAlunosEMatriculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiofpimentel/gestaoAlunosEMatriculas/GestaoDeAlunosEMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiofpimentel.gestaoAlunosEMatriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeAlunosEMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
